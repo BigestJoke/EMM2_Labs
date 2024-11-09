d3c25e6db53ec9bfb7f3fc31fa0a11c5dcc89a70
Nikita Kuznecov <nikita_kuznecov2004@mail.ru>
2024-11-09 23:20:46 +0700

    ParEstimation

--- Emm3_Lab.R
+++ Emm3_Lab.R
@@ -1,6 +1,4 @@
-# Установка необходимых пакетов
-if (!require("tseries")) install.packages("tseries", dependencies = TRUE)
-if (!require("rugarch")) install.packages("rugarch", dependencies = TRUE)
+#Libraries
 library(tseries)
 library(rugarch)
 
@@ -24,8 +22,8 @@ ARARCH = function(n){
 
 #ARARCH&Visualization----
 AArch = ARARCH(n)
-plot(AArch$x, type = "l", col = "darkred", main = "Генерированный процесс AR(2)ARCH(3)",
-     xlab = "Время", ylab = "Значение x")
+plot(AArch$x, type = "l", col = "darkred", main = "AR(2)ARCH(3)",
+     xlab = "t", ylab = "x")
 
 #Test&Train&Visualization
 trainN = floor(20/21 * n)
@@ -35,6 +33,27 @@ train = AArch$x[1:trainN]
 test = AArch$x[(trainN + 1):n]
 
 plot(train, type = "l", col = "darkblue", main = "Обучающая выборка", 
-     xlab = "Время", ylab = "x_train")
+     xlab = "t", ylab = "train")
 plot(test, type = "l", col = "black", main = "Тестовая выборка", 
-     xlab = "Время", ylab = "x_test")
+     xlab = "t", ylab = "test")
+
+
+
+#ParEstimation----
+ARest = arima(train, order = c(2, 0, 0))
+thetaEst = coef(ARest)[1:2]
+cat("Оцененные параметры для AR(2):", thetaEst, "\n")
+
+ARresid = residuals(ARest)
+plot(ARresid, type = "l", col = "black", main = "Остатки AR(2)",
+     xlab = "t", ylab = "Остатки")
+
+ARCHMod = garch(ARresid, order = c(0, 3))
+ARCHest = coef(ARCHMod)
+cat("Оцененные параметры для ARCH(3):", ARCHest, "\n")
+
+Varest = fitted(ARCHMod)[,1]^2
+plot(Varest, type = "l", col = "red", main = "Оцененная дисперсия ARCH(3)",
+     xlab = "t", ylab = "Дисперсия")
+
+
