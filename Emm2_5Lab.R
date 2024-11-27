7756aba0b20f08393978971b5ac05ad96ad1561c
Nikita Kuznecov <nikita_kuznecov2004@mail.ru>
2024-11-27 12:41:33 +0700

    ConfIntervals

--- Emm2_5Lab.R
+++ Emm2_5Lab.R
@@ -30,4 +30,10 @@ plot(tp, B, type = "l", col = "blue", lwd = 1,
 #Ensemble
 EnsemblePlot(200, delta, steps)
 
+#Confidence intervals
+sigma = 3 * sqrt(tp)
+lines(tp, sigma, col = "red", lwd = 2, lty = 2)
+lines(tp, -sigma, col = "red", lwd = 2, lty = 2)
+
+
 
