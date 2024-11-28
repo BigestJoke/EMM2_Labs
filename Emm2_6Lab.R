f1f7ab0418ea90584fcc016d074441a826baf4af
Nikita Kuznecov <nikita_kuznecov2004@mail.ru>
2024-11-28 22:41:07 +0700

    ParEstimate

--- Emm2_6Lab.R
+++ Emm2_6Lab.R
@@ -2,7 +2,7 @@
 S0 = 100
 a = 0.5
 sigma = 0.8
-delta = 0.01
+delta = 0.0001
 steps = 1000
 
 BrownSimulate = function(delta, steps) {
@@ -12,16 +12,27 @@ BrownSimulate = function(delta, steps) {
 }
 GeoBrSimulation = function(S0,a, sigma, delta, steps,B){
   S = numeric(steps + 1)  
-  S[1] = S0  # устанавливаем начальное значение
+  S[1] = S0  
   
   for (i in 1:steps) {
-    # Используем значения броуновского движения из предыдущего кода
     S[i + 1] = S0 * exp((a - sigma^2 / 2) * (i * delta) + sigma * B[i + 1])  
   }
   return(S)
 }
+IncrCalc = function(S) {
+  diff(log(S)) 
+}
+ParEst = function(increments, delta) {
+  m_ = mean(increments)
+  D = var(increments)
+  
+  sigma_ = D / delta  
+  a_ = m_ / delta + sigma_ / 2  
+  
+  return(list(a_ = a_, sigma_ = sigma_))
+}
 
-#Br-Geo-BrSimulation----
+#Br&GeoBrSimulation----
 tp = seq(0, steps * delta, by = delta)
 B = BrownSimulate(delta, steps)
 S = GeoBrSimulation(S0,a, sigma, delta, steps,B)
@@ -30,6 +41,11 @@ S = GeoBrSimulation(S0,a, sigma, delta, steps,B)
 plot(S, type = "l", col = "blue", xlab = "t", ylab = "S(t)", lwd = "1",
      main = "Геометрическое броуновское движение")
 
+increments = IncrCalc(S)
+params = ParEst(increments, delta)
 
+#Output
+cat("Оценка параметра a :", params$a_, "\n")
+cat("Оценка параметра sigma:", params$sigma_, "\n")
 
   
