78b64dbe52fcd07ae061976912da335f4734bbbe
Nikita Kuznecov <nikita_kuznecov2004@mail.ru>
2024-12-03 23:43:09 +0700

    Visualization

--- Emm2_7Lab.R
+++ Emm2_7Lab.R
@@ -4,6 +4,7 @@ set.seed(123)
 t = 50
 lyambda = 2
 n = 1000
+
 InsCaseSim = function(t, lyambda, n) {
   N_t = numeric(n)
   for (i in 1:n) {
@@ -21,4 +22,12 @@ InsCaseSim = function(t, lyambda, n) {
 
 #insurance-case-modeling----
 N_t = InsCaseSim(t, lyambda, n)
+
+#Visualization----
+hist(N_t, probability = TRUE, main = "Гистограмма + ф-ция вероятности Пуассона",
+     xlab = "N_t", ylim = c(0, 0.05))
+xp = 0:max(N_t)
+yp = dpois(xp, lambda = lyambda * t)
+points(xp, yp, col = "blue", pch = 16)
+
 
