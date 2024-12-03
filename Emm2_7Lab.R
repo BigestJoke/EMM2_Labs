9b0a72ac48cd56f07e2690e69555a878847f9d75
Nikita Kuznecov <nikita_kuznecov2004@mail.ru>
2024-12-04 00:24:07 +0700

    Capital-insurance-company

--- Emm2_7Lab.R
+++ Emm2_7Lab.R
@@ -6,10 +6,10 @@ lyambda = 2
 n = 1000
 
 u0 = 50
-t_max <- 100
-mu <- 1
-c_safe <- 3
-c_risky <- 1.5
+tmax = 300
+m = 1
+cSafely = 3
+cRisky = 1.5
 
 InsCaseSim = function(t, lyambda, n) {
   N_t = numeric(n)
@@ -25,20 +25,20 @@ InsCaseSim = function(t, lyambda, n) {
   }
   N_t
 }
-simulate_capital_process <- function(U0, c, lambda, mu, t_max) {
-  capital <- U0
-  times <- 0
-  process <- list(times = numeric(), capital = numeric())
+CapitalProcSim = function(u0, c, lyambda, m, tmax) {
+  capital = u0
+  t = 0
+  process = list(times = numeric(), capital = numeric())
   
-  while (times < t_max) {
-    tau <- rexp(1, rate = lambda)
-    times <- times + tau
-    if (times >= t_max) break
+  while (t < tmax) {
+    tt = rexp(1, rate = lyambda)
+    t = t + tt
+    if (t >= tmax) break
     
-    payout <- rexp(1, rate = 1 / mu)
-    capital <- capital + c * tau - payout
-    process$times <- c(process$times, times)
-    process$capital <- c(process$capital, capital)
+    payout = rexp(1, rate = 1 / m)
+    capital = capital + c * tt - payout
+    process$t = c(process$t, t)
+    process$capital = c(process$capital, capital)
   }
   
   process
@@ -54,6 +54,15 @@ curve(((lyambda * t)^x / factorial(x)) * exp(-lyambda * t),
       add = TRUE, col = "blue", lwd = 2)
 
 
+#capital-insurance-company----
+SafeProc = CapitalProcSim(u0, lyambda,cSafely, m, tmax) #a
+Risky = CapitalProcSim(u0, lyambda,cRisky, m, tmax) #b
 
+plot(process_safe$t, process_safe$capital, type = "s", col = "blue",
+     main = "Процесс капитала A)", 
+     xlab = "Время", ylab = "Капитал")
 
+plot(process_risky$t, process_risky$capital, type = "s", col = "red",
+     main = "Процесс капитала B)", 
+     xlab = "Время", ylab = "Капитал")
 
